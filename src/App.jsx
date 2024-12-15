632d8bad947f25f4d038253a134dcd1974a3e5b7
Viral Vaghela <viralvaghela8577@gmail.com>
2024-12-15 12:07:57 +0530

    fix - conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,20 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Route, Routes  } from 'react-router-dom';;
-import {HomePage , MobileVerificationPage , OtpVerificationPage , DocumentUploadPage} from './Pages/PageIndex';
-=======
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./Pages/HomePage";
-import MobileVerificationPage from "./Pages/MobileVerificationPage";
-import OtpVerificationPage from "./Pages/OTPAuth";
-import DocumentUpload from "./Pages/DocumentUpload";
+import {
+  HomePage,
+  MobileVerificationPage,
+  OtpVerificationPage,
+  DocumentUploadPage,
+} from "./Pages/PageIndex";
 import { Toaster } from "react-hot-toast";
->>>>>>> 0a6f5cb (update code structure, ui, otp toast message)
 function App() {
   return (
     <>
       <Toaster />
       <Routes>
-<<<<<<< HEAD
-        <Route path='/' element={<MobileVerificationPage />} />
-        <Route path='/Home' element={<HomePage />} />
-        <Route path='/otp-verify' element={<OtpVerificationPage />} />
-        <Route path='/upload-document' element={<DocumentUploadPage />} />
-=======
         <Route path="/" element={<MobileVerificationPage />} />
         <Route path="/bikey" element={<HomePage />} />
         <Route path="/otp-verify" element={<OtpVerificationPage />} />
-        <Route path="/upload-document" element={<DocumentUpload />} />
->>>>>>> 0a6f5cb (update code structure, ui, otp toast message)
+        <Route path="/upload-document" element={<DocumentUploadPage />} />
       </Routes>
     </>
   );
